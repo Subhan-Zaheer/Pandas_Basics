645e88c9e2683f55010d78c359de21d2f73bcbb1
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-08-04 14:50:12 +0500

    Some basic functions after reading csv file

---
+++ raading_csv_file.py
@@ -0,0 +1,10 @@
+"""
+Following are some functions to read data from csv file and to do some processing.
+
+1. read_csv() : This function is actually used to read data from CSV files into a Pandas Data frame object.
+2. .info() : This function will give us basic info about rows, columns and data types.
+3. .describe() : This will give us statistical info about numerical columns.
+4. .columns() : Get list of column names.
+5. .shape() : Get the number of columns and rows as tuple.
+
+"""
